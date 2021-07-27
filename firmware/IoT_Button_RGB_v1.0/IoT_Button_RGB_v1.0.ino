f4ba5b96cca94cb84807018e21f7b56797e1f13a
Salvatore Raccardi <36445663+salvatoreraccardi@users.noreply.github.com>
2021-07-27 08:45:46 +0200

    Update IoT_Button_RGB_v1.0.ino

--- firmware/IoT_Button_RGB_v1.0/IoT_Button_RGB_v1.0.ino
+++ firmware/IoT_Button_RGB_v1.0/IoT_Button_RGB_v1.0.ino
@@ -1,49 +1,35 @@
-#define BUTTON_PIN_BITMASK 0x8004
-RTC_DATA_ATTR int bootCount = 0;
+RTC_DATA_ATTR int boot = 0;
 
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN    23
 #define LED_COUNT 5
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel leds(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
-  strip.begin(); 
-  strip.setBrightness(0);          
-  strip.show();           
-  
-  ++bootCount;
-  //esp_sleep_enable_ext0_wakeup(GPIO_NUM_15,1);
-  esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,ESP_EXT1_WAKEUP_ANY_HIGH);
-
+  //deepsleep
+  ++boot;
+  // 0x8000 => 2^15 in hex => GPIO15
+  esp_sleep_enable_ext1_wakeup(0x8000,ESP_EXT1_WAKEUP_ANY_HIGH); 
+  // WS2812B-2020 init
+  leds.begin(); 
+  leds.setBrightness(0);          
+  leds.show();   
 }
 
 
-void loop() {
-  strip.setBrightness(50); 
-  //rainbow(8);
-  color(250);
-  strip.show();
-  strip.setBrightness(0); 
-  strip.show();           
+void loop() {  
+  rgb(250);          
   esp_deep_sleep_start();
 }
 
-void color(int wait){
-  for(int i = 0; i < strip.numPixels(); i++){
-     strip.setPixelColor(i, 0, 255, 0);
-     strip.show();
+void rgb(int wait){
+  leds.setBrightness(50);
+  for(int i = 0; i < leds.numPixels(); i++){
+     leds.setPixelColor(i, 0, 255, 0);
+     leds.show();
      delay(wait); 
   }
-}
-
-void rainbow(int wait) { 
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    for(int i=0; i<strip.numPixels(); i++) { 
-      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
-      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
-    }
-    strip.show();
-    delay(wait); 
-  }
+  leds.setBrightness(0); 
+  leds.show();  
 }
